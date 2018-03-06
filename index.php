d168f2775c32bd9bded533c5eff4ce0939e7d2ae
Raniaih <35455486+Raniaih@users.noreply.github.com>
2018-03-06 11:59:39 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+$title = "PAULVAQUIZZ";
+require 'views/header.php'; ?>
+
+<script type="text/javascript">
+		$('.nav-item').removeClass("active");
+		$('#home').addClass("active");
+
+</script>
+</body>
+</html>
