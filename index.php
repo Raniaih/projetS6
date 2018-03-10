917d3a683181dd930d1d9912224e297b385e1a67
Raniaih <35455486+Raniaih@users.noreply.github.com>
2018-03-10 17:48:15 +0000

    Add files via upload
    +logo

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 $title = "PAULVAQUIZZ";
 require 'views/header.php'; ?>
 
+<center><img src="img/logo.png" alt= "logo"/></center>
+
 <script type="text/javascript">
 		$('.nav-item').removeClass("active");
 		$('#home').addClass("active");
